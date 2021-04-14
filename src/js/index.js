5bd5d7746ae03f4f885f8a3f337a2761a18f9073
rana0731 <oexeanar8a@gmail.com>
2021-04-15 01:30:40 +0800

    docs folder uusgelee

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,6 @@
 require('@babel/polyfill');
-import axios from 'axios';
+import Search from './model/Search';
 
-async function doSearch(search) {
-    try{
-        let result = await axios('https://forkify-api.herokuapp.com/api/search?q=' + search);
-        const recipes = result.data.recipes;
-        console.log(recipes);
+let search = new Search('pasta');
 
-        result = await axios('https://forkify-api.herokuapp.com/api/search?q=' + recipes[1].recipe_id);
-        console.log(result);
-    }catch(error){
-        alert('Асуудал гарлаа: ' + error);
-
-    }
-    
-}
-
-doSearch('pizza');
+search.doSearch().then(r => console.log('Үр дүн:' + r));
