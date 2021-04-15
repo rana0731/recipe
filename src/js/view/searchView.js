e3fdc0cb40945b4fbfa85ccdd013fca9e9e08c1c
rana0731 <oexeanar8a@gmail.com>
2021-04-15 18:54:41 +0800

    huudasnii button

--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -11,7 +11,7 @@ const renderRecipe = recipe => {
                 </figure>
                 <div class="results__data">
                     <h4 class="results__name">${recipe.title}</h4>
-                    <p class="results__author">${recipe.puplisher}</p>
+                    <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
         </li>
@@ -27,10 +27,46 @@ export const clearSearchQuery = () => {
 
 export const clearSearchResult = () => {
     elements.searchResultList.innerHTML = "";
-}
+    elements.pageButtons.innerHTML = "";
+};
 
 export const getInput = () =>  elements.searchInput.value;
-export const renderRecipes = recipes => {
-    // undefined
-    recipes.forEach(renderRecipe);
+
+export const renderRecipes = (recipes, currentPage = 1, resPerPage = 10) => {
+
+    // Хайлтын үр дүнг хуудаслаж үзүүлэх
+    // page = 2, start = 10, end = 20
+    const start = (currentPage-1) * resPerPage;
+    const end = currentPage * resPerPage;
+
+    recipes.slice(start, end).forEach(renderRecipe);
+
+    // Хуудаслалтын товчуудыг гаргаж ирэх
+    const totalPages = Math.ceil(recipes.length / resPerPage); // math.ceil нв 4.2 ==> 5
+    renderButtons(currentPage, totalPages);
+};
+
+// type ===>'prev', 'next'
+const createButton = (page, type, direction) => 
+`<button class="btn-inline results__btn--${type}" data-goto=${page}>
+<span>Хуудас ${page}</span>
+<svg class="search__icon">
+    <use href="img/icons.svg#icon-triangle-${direction}"></use>
+</svg>
+</button>`;
+const renderButtons = (currentPage, totalPages) => {
+    let buttonHTML ;
+    if(currentPage === 1 && totalPages > 1){
+        // 1-р хуудсан дээр байна, 2-р хуудас гэдэг товчийг гарга.
+        buttonHTML = createButton(2, "next", "right");
+    } else if(currentPage < totalPages){
+        // Өмнөх болон дараачийн хуудас руу шилжих товчуудыг үзүүл.
+        buttonHTML = createButton(currentPage - 1, "prev", "left");
+        buttonHTML += createButton(currentPage + 1, "next", "right");
+    } else if(currentPage === totalPages){
+        // Хамгийн сүүлийн хуудас дээр байна. Өмнөх рүү шилжүүлэх товчийг л үзүүлнэ.
+        buttonHTML = createButton(currentPage - 1, "prev", "left");
+    }
+
+    elements.pageButtons.insertAdjacentHTML('afterbegin', buttonHTML);
 };
