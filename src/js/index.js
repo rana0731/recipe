ff40c0e76fa5131f6e6f5c68646385bc8342b6ed
rana0731 <oexeanar8a@gmail.com>
2021-04-23 01:11:15 +0800

    goliin heseg, minii sags, hicheel 125

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,9 @@ import Search from "./model/Search";
 import {elements, renderLoader, clearLoader} from "./view/base";
 import * as searchView from "./view/searchView";
 import Recipe from "./model/Recipe";
+import List from "./model/List";
+import * as listView from "./view/listView";
+import {renderRecipe, clearRecipe, highlightSelectedRecipe} from './view/recipeView';
 
 /**
  * Web app төлөв
@@ -13,6 +16,10 @@ import Recipe from "./model/Recipe";
  */
 
 const state = {};
+
+/**
+ * Хайлтын контроллер (Model ==> Controller <== View)
+ */
 
 const controlSearch = async () => {
     // 1. Вэбээс хайлтын түлхүүр үгийг гаргаж авна.
@@ -54,5 +61,77 @@ elements.pageButtons.addEventListener("click", e => {
     }
 });
 
-const r = new Recipe(47746);
-r.getRecipe();
+/**
+ * Жорын контроллер
+ */ 
+
+const controlRecipe = async () => {
+    // 1) URL - аас ID -ийг салгах
+    const id = window.location.hash.replace('#', '');
+
+    //  URL дээр ID байгаа эсэхийг шалгана.
+    if(id){
+        // 2) Жорын моделийг үүсгэж өгнө.
+        state.recipe = new Recipe(id);
+
+        // 3) UI дэлгэцийг бэлтгэнэ.
+        clearRecipe();
+        renderLoader(elements.recipeDiv);
+        highlightSelectedRecipe(id);
+
+        // 4) Жороо татаж авчирна.
+        await state.recipe.getRecipe();
+
+        // 5) Жорыг гүйцэтгэх хугацаа болон орцыг тооцоолно.
+        clearLoader();
+        state.recipe.calcTime();
+        state.recipe.calcHuniiToo();
+
+        // 6) Жороо дэлгэцэнд гаргана.
+        renderRecipe(state.recipe);
+    }
+}; 
+
+// window.addEventListener('hashchange', controlRecipe);
+// window.addEventListener('load', controlRecipe);
+
+['hashchange', 'load'].forEach(e => window.addEventListener(e, controlRecipe));
+/**
+ * Найрлаганы контроллер
+ */ 
+
+const controlList = () => {
+    // Найрлаганы моделийг үүсгэнэ.
+    state.list = new List();
+
+    // Өмнө нь харагдаж байсан найрлагуудыг дэлгэцээс зайлуулна.
+    listView.clearItems();
+
+    // Уг модел рүү одоо харагдаж байгаа жорны бүх найрлагыг авч хийнэ.
+    state.recipe.ingredients.forEach(n => {
+
+        // Тухайн  найрлагыг модел рүү хийнэ.
+        const item = state.list.addItem(n);
+
+        // Тухайн найрлагыг дэлгэцэнд гаргана.
+        listView.renderItem(item);
+    });
+};
+
+elements.recipeDiv.addEventListener('click', e => {
+    if(e.target.matches('.recipe__btn, .recipe__btn *')) {
+        controlList();
+    }
+});
+
+elements.shoppingList.addEventListener('click', e => {
+    // Клик хийсэн li элемэнтийн data-itemid аттрибутыг шүүж гаргаж авах. 
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+
+    // Олдсон ID-тэй орцыг моделоос устгана.
+    state.list.deleteItem(id);
+    
+    // Дэлгэцээс ийс ID-тэй орцыг олж бас устгана.
+    listView.deleteItem(id);
+
+});
---
+++ src/js/model/List.js
@@ -0,0 +1,25 @@
+import uniqid from 'uniqid';
+
+export default class List {
+    constructor() {
+        this.items = [];
+    }
+
+    deleteItem(id) {
+        // id гэдэг ID-тэй орцын индексийг массиваас хайж олно.
+        const index = this.items.findIndex(el => el.id === id);
+
+        // Уг индекс дээрх элементийг массиваас устгана.
+        this.items.splice(index, 1);
+    }
+
+    addItem(item) {
+        let newItem = {
+            id: uniqid(),
+            item // item: item gesnii tovchlol
+        };
+        this.items.push(newItem);
+
+        return newItem;
+    }
+};
